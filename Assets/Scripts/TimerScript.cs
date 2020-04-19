6214eb8fcf3c8f99c9879ef0fdd662c78a660d3a
Aleksey Frolov <Aleksey2093@outlook.com>
2020-04-20 01:21:11 +0300

    init

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour {
+    public UnityEngine.UI.Text textOfTimer;
+    public UnityEngine.UI.Text textOfButton;
+    public UnityEngine.UI.Button button;
+
+    // Start is called before the first frame update
+    void Start() {
+        button.onClick.AddListener(ButtonClick);
+    }
+
+    public bool timerStatus = false;
+    public float timerSecOld;
+
+    public void ButtonClick() {
+        timerStatus = !timerStatus;
+        timerSecOld = Time.time;
+        textOfButton.text = timerStatus ? "Остановить" : "Запустить";
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!timerStatus) return;
+        float sec = Time.time - timerSecOld;
+        textOfTimer.text = $"{sec:f1} c.";
+    }
+}
+
+
+
+
